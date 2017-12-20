4bf06dfaaee7c5ae9bc2c145f7ce0829aa58bb68
Elouan <elouan.06@hotmail.fr>
2017-12-20 14:22:10 +0100

    methode tri

--- src/com/knadr/util/Tri.java
+++ src/com/knadr/util/Tri.java
@@ -1,4 +1,28 @@
 package com.knadr.util;
 
+import java.util.ArrayList;
+
 public class Tri {
+    public static void TriScore(ArrayList<String> pseudo, ArrayList<Integer> bestScores) {
+        int compteur=1;
+        String pseudot;
+        int  scoret;
+        while (compteur>0){
+            compteur=0;
+            for(int i=0;i<9;i++){
+                if(bestScores.get(i)<bestScores.get(i+1)){
+                    compteur++;
+
+                    pseudot=pseudo.get(i);
+                    pseudo.set(i,pseudo.get(i+1));
+                    pseudo.set(i+1,pseudot);
+
+                    scoret=bestScores.get(i);
+                    bestScores.set(i,bestScores.get(i+1));
+                    bestScores.set(i+1,scoret);
+                }
+        }
+
+        }
+    }
 }
